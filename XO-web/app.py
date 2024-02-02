2ac9c5fa2cb3be032c48b7aaaf5177565a8242b0
Nattapon <jammy51000@gmail.com>
2024-02-02 16:52:43 +0700

    can check history and create table of playing history

--- XO-web/app.py
+++ XO-web/app.py
@@ -22,7 +22,48 @@ def index():
     data = db.mycollection.find()
     return render_template('index.html', data=data)
 
+#  ----------API เก็บข้อมูลลง Database--------------
+#  เก็บประวัติการเล่นบน database
+@app.route('/api/play_round', methods=['POST'])
+def play_round():
+    # รับข้อมูลที่ส่งมาจากแอพพลิเคชัน (รหัสวิชา)
+    data = request.json
 
+    id_r = data.get('id_round')
+    list_x = data.get('list_x')
+    list_o = data.get('list_o')
+    size = data.get('size')
+
+    filter_condition = {'_id': id_r}
+
+    # ตรวจสอบว่าข้อมูลถูกส่งมาหรือไม่
+    if not list_x or not list_o :
+        return jsonify({'message': 'ข้อมูลไม่ครบถ้วน'}), 400
+
+    # กำหนดเงื่อนไขในการค้นหาเอกสารที่มี '_id' เท่ากับ '030'
+    # filter_condition = {'_id': '030'}
+
+    # กำหนดข้อมูลที่จะใช้ในการอัปเดตหรือสร้างเอกสารใหม่
+    update_data = {
+        '_id': id_r,  # ระบุ '_id' เพื่อหาเอกสารที่มีหรือสร้างใหม่
+        'list_x': list_x,
+        'list_o': list_o,
+        'size': size
+    }
+
+    # ใช้คำสั่ง update_one เพื่ออัปเดตหรือสร้างเอกสารใน MongoDB
+    db.play_round.update_one(filter_condition, {'$set': update_data}, upsert=True)
+
+    return jsonify({'message': 'บันทึกข้อมูลสำเร็จ'}), 200
+
+
+#  ส่งค่าผลการเล่นให้แสดงบนหน้า
+@app.route('/api/play_round_display', methods=['GET'])
+def display_play_round():
+    # ดึงข้อมูลจาก MongoDB
+    data = list(db.play_round.find({}))
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- XO-web/init-db.js
+++ XO-web/init-db.js
@@ -1,1 +1,6 @@
 db = db.getSiblingDB("test_db");
+
+// db.createCollection("play_round");
+
+
+db.play_round.insertOne()
